64956e4b048cba83406b8152e00e3d1cfa10568e
Brian Novak <tajrkala@gmail.com>
2020-01-04 18:59:22 -0600

    Split into several functions

--- files2Joplin.py
+++ files2Joplin.py
@@ -1,13 +1,21 @@
 '''
-Create a RAW directory for import into Joplin using the files in an given attachment directory.
+Create a RAW directory for import into Joplin using the files in a given attachment directory.
 Sub-directories are ignored.
 The RAW directory is named joplin and is created as a sub-directory of the attachment directory.
 One note is created for each file where the file name not including the extension is the title and
 the body consists of only a link to the attached file.
 
 Command line inputs:
-    The Joplin directory is needed to check if randomly generated file names already exist.
-    The attachment directory with the files to add to the RAW directory.
+1) The absolute path for the Joplin directory.
+   It is the directory you are syncing to, not the local directory.
+   This is needed to check if randomly generated file names already exist.
+2) The absolute path for the attachment directory with the files to add to the RAW directory.
+
+**WARNING: If you plan on editing attachments, you should check that Joplin actually syncs
+attachments after they are edited before using this. I have had problems with that in
+Joplin 1.0.175. If your attachments are not syncing after editing, an alternative would be to link
+to the files directly using file:// in front of the file paths. I may add an option for this if the
+next version of Joplin does not fix this.**
 '''
 
 from datetime import datetime, timezone
@@ -17,125 +25,169 @@ import os
 import shutil
 import sys
 
-# Inputs
-joplin_directory = sys.argv[1]
-files_directory = sys.argv[2]
 
-# Change directory
-os.chdir(files_directory)
-
-# List of files
-file_list = glob.glob('*.*')
-
-# Make RAW directory
-try:
-    os.mkdir('joplin')
-    os.mkdir('joplin/resources')
-except FileExistsError:
-    # Stop if joplin directory exists. Don't just delete since user may not have imported it yet.
-    print('joplin directory exists. Import it if necessary, remove it, and run again.')
-
-# Current time in Joplin format. Might use file attributes for times instead of just current time?
-now = datetime.now(timezone.utc).isoformat()[:23] + 'Z'
-
-# Loop over attachment files
-for file in file_list:
+def split_file_name(file):
+    '''
+    Split file name into name and extension
+    '''
 
     # Attachment file name and extension
     split = file.split('.')
     extension = '.' + split[-1]
     file_name = '.'.join(split[:-1])
 
+    return (extension, file_name)
+
+
+def write_attachment_file(file, rand_attach, now):
+    '''
+    Move attachment to RAW directory. Write attachment markdown file.
+    Inputs:
+        file: Attachment File name before adding to Joplin
+        rand_attach: Attachment file name after adding to Joplin
+        extension: Attachment file extension
+        now: Current date & time
+    '''
+
+    (extension, _) = split_file_name(file)
+
     # Attachment file size
     file_size = os.path.getsize(file)
 
-    # Attachment markdown file
-
-    # Random markdown file name
-    rand_attach = os.urandom(16).hex()
-    while os.path.exists(joplin_directory + '/' + rand_attach + '.md'):
-        rand_attach = os.urandom(16).hex()
-
-    # Move attachment to RAW directory.
+    # Move file
     shutil.move(file, 'joplin/resources/' + rand_attach)
 
     # Write markdown file
-    with open('joplin/' + rand_attach + '.md', 'w') as f:
+    with open('joplin/' + rand_attach + '.md', 'w') as fid:
 
-        f.write(file + '\n\n')
-        f.write('id: ' + rand_attach + '\n')
+        fid.write(file + '\n\n')
+        fid.write('id: ' + rand_attach + '\n')
 
         try:
             mimetype = mimetypes.types_map[extension]
         except KeyError:
             mimetype = ''
 
-        f.write('mime: ' + mimetype + '\n')
+        fid.write('mime: ' + mimetype + '\n')
 
-        f.write('filename: ' + file + '\n')
+        fid.write('filename: ' + file + '\n')
 
-        f.write('created_time: ' + now + '\n')
-        f.write('updated_time: ' + now + '\n')
-        f.write('user_created_time: ' + now + '\n')
-        f.write('user_updated_time: ' + now + '\n')
+        fid.write('created_time: ' + now + '\n')
+        fid.write('updated_time: ' + now + '\n')
+        fid.write('user_created_time: ' + now + '\n')
+        fid.write('user_updated_time: ' + now + '\n')
 
-        f.write('file_extension: \n')
+        fid.write('file_extension: \n')
 
-        f.write('encryption_cipher_text: \n')
-        f.write('encryption_applied: 0\n')
-        f.write('encryption_blob_encrypted: 0\n')
+        fid.write('encryption_cipher_text: \n')
+        fid.write('encryption_applied: 0\n')
+        fid.write('encryption_blob_encrypted: 0\n')
 
-        f.write('size: ' + str(file_size) + '\n')
-        f.write('type_: 4')
+        fid.write('size: ' + str(file_size) + '\n')
+        fid.write('type_: 4')
 
-    # Markdown file with link to attachment
+
+def write_note_file(joplin_directory, file, rand_attach, now):
+    '''
+    Write note markdown file with link to attachment.
+    Inputs:
+        file: Attachment file name before adding to Joplin without extension
+        rand_md: Random markdown file name for note
+        rand_attach: Attachment file name after adding to Joplin
+        extension: Attachment file extension
+        now: Current date & time
+    '''
+
+    (extension, file_name) = split_file_name(file)
 
     # Random markdown file name
     rand_md = os.urandom(16).hex()
     while os.path.exists(joplin_directory + '/' + rand_md + '.md'):
         rand_md = os.urandom(16).hex()
 
-    # Write markdown file
-    with open('joplin/' + rand_md + '.md', 'w') as f:
+    with open('joplin/' + rand_md + '.md', 'w') as fid:
 
         # Title and link to attachment.
         # Image types which will display in Joplin are given image links.
-        f.write(file_name + '\n\n')
+        fid.write(file_name + '\n\n')
         if extension in ['.png', '.jpeg', '.jpg']:
-            f.write('![' + file + '](:/' + rand_attach + ')\n\n')
+            fid.write('![' + file + '](:/' + rand_attach + ')\n\n')
         else:
-            f.write('[' + file + '](:/' + rand_attach + ')\n\n')
+            fid.write('[' + file + '](:/' + rand_attach + ')\n\n')
 
         # Other data
-        f.write('id: ' + rand_md + '\n')
-        f.write('parent_id: \n') # ' + parent_id + '
+        fid.write('id: ' + rand_md + '\n')
+        fid.write('parent_id: \n') # ' + parent_id + '
 
-        f.write('created_time: ' + now + '\n')
-        f.write('updated_time: ' + now + '\n')
+        fid.write('created_time: ' + now + '\n')
+        fid.write('updated_time: ' + now + '\n')
 
-        f.write('is_conflict: 0\n')
+        fid.write('is_conflict: 0\n')
 
-        f.write('latitude: 0.00000000\n')
-        f.write('longitude: 0.00000000\n')
-        f.write('altitude: 0.0000\n')
+        fid.write('latitude: 0.00000000\n')
+        fid.write('longitude: 0.00000000\n')
+        fid.write('altitude: 0.0000\n')
 
-        f.write('author: \n')
-        f.write('source_url: \n')
+        fid.write('author: \n')
+        fid.write('source_url: \n')
 
-        f.write('is_todo: 0\n')
-        f.write('todo_due: 0\n')
-        f.write('todo_completed: 0\n')
+        fid.write('is_todo: 0\n')
+        fid.write('todo_due: 0\n')
+        fid.write('todo_completed: 0\n')
 
-        f.write('source: joplin-desktop\n')
-        f.write('source_application: net.cozic.joplin-desktop\n')
-        f.write('application_data:\n')
-        f.write('order: 0\n')
+        fid.write('source: joplin-desktop\n')
+        fid.write('source_application: net.cozic.joplin-desktop\n')
+        fid.write('application_data:\n')
+        fid.write('order: 0\n')
 
-        f.write('user_created_time: ' + now + '\n')
-        f.write('user_updated_time: ' + now + '\n')
+        fid.write('user_created_time: ' + now + '\n')
+        fid.write('user_updated_time: ' + now + '\n')
 
-        f.write('encryption_cipher_text: \n')
-        f.write('encryption_applied: 0\n')
+        fid.write('encryption_cipher_text: \n')
+        fid.write('encryption_applied: 0\n')
 
-        f.write('markup_language: 1\n')
-        f.write('type_: 1')
+        fid.write('markup_language: 1\n')
+        fid.write('type_: 1')
+
+
+def main(joplin_directory, files_directory):
+    '''
+    main
+    '''
+
+    # Change directory
+    os.chdir(files_directory)
+
+    # List of files
+    file_list = glob.glob('*.*')
+
+    # Make RAW directory
+    try:
+        os.mkdir('joplin')
+        os.mkdir('joplin/resources')
+    except FileExistsError:
+        # Stop if joplin directory exists. Don't just delete since user may not have imported yet.
+        print('joplin directory exists. Import it if necessary, remove it, and run again.')
+
+    # Current time in Joplin format. Might use file upated times instead of just current time?
+    now = datetime.now(timezone.utc).isoformat()[:23] + 'Z'
+
+    # Loop over attachment files
+    for file in file_list:
+
+        # Random markdown file name for attachment
+        rand_attach = os.urandom(16).hex()
+        while os.path.exists(joplin_directory + '/' + rand_attach + '.md'):
+            rand_attach = os.urandom(16).hex()
+
+        write_attachment_file(file, rand_attach, now)
+        write_note_file(joplin_directory, file, rand_attach, now)
+
+
+if __name__ == '__main__':
+
+    # Inputs
+    JOPLIN_DIRECTORY = sys.argv[1]
+    FILES_DIRECTORY = sys.argv[2]
+
+    main(JOPLIN_DIRECTORY, FILES_DIRECTORY)
